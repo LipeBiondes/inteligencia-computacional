75699bada7f184491aa66146508d30cd8045ed86
Caio Martins do Nascimento <martinscaio29@gmail.com>
2024-04-23 10:55:45 -0300

    add some new grafic

--- randomForest-por-dia.py
+++ randomForest-por-dia.py
@@ -10,7 +10,7 @@ X_treino = dados_treino[['ano', 'mes', 'dia']]
 y_treino = dados_treino['Acidentes']
 
 # Instanciar e treinar o modelo Random Forest
-modelo = RandomForestRegressor(n_estimators=100, max_depth=8, random_state=42)  # Defina o número de estimadores (n_estimators) e profundidade máxima da árvore (max_depth)
+modelo = RandomForestRegressor(n_estimators=100, max_depth=8, random_state=42)
 modelo.fit(X_treino, y_treino)
 
 # Fazer previsões para os dados de teste (ano, mês, dia)
@@ -27,11 +27,13 @@ dados_reais_agrupados = dados_teste.groupby('mes')['Acidentes'].sum().reset_inde
 # Agrupar as previsões por mês
 previsoes_agrupadas = dados_teste.groupby('mes')['Previsoes'].sum().reset_index()
 
-# Plotar o gráfico com os resultados
+# Plotar o gráfico de linha
+plt.figure(figsize=(10, 6))
 plt.plot(dados_reais_agrupados['mes'], dados_reais_agrupados['Acidentes'], label='Dados Reais', marker='o')
 plt.plot(previsoes_agrupadas['mes'], previsoes_agrupadas['Previsoes'], label='Previsões', marker='x')
 plt.xlabel('Mês')
 plt.ylabel('Número de Acidentes')
 plt.title('Comparação entre Dados Reais e Previsões de Número de Acidentes por Mês (2023) - Random Forest por Dia')
 plt.legend()
+plt.grid(True)
 plt.show()
